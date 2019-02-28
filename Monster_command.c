a784f8f51ed8707251daa10abd7d841855d42d69
Mahiro_K <mahirokomatuo0102@gmail.com>
2019-02-28 11:58:23 +0900

    add:

--- Monster_command.c
+++ Monster_command.c
@@ -1,28 +1,51 @@
 #include <gtk/gtk.h>
 #include "stdlib.h"
 #include "time.h"
-
-void Enemy_attack_Entrance(void *d)
-{
-    Pointer_Addres *k = d;
-}
+#include <structs.h>
 
 void Enemy_power_attck(void *d)
 {
-    Pointer_Addres *k = d;
+    appearance_character *k = d;
 }
 
 void Enemy_magic_attack(void *d)
 {
-    Pointer_Addres *k = d;
+    appearance_character *k = d;
 }
 
 void Enemy_healing(void *d)
 {
-    Pointer_Addres *k = d;
+    appearance_character *k = d;
 }
 
 void Enemy_item(void *d)
 {
-    Pointer_Addres *k = d;
+    appearance_character *k = d;
 }
+
+void Enemy_attack_Entrance(void *d)
+{
+    appearance_character *k = d;
+    srand((unsigned)time(NULL));
+    int a = rand() % 4;
+
+    switch (a)
+    {
+    case 0:
+        Enemy_power_attck(k);
+        break;
+
+    case 1:
+        Enemy_magic_attack(k);
+        break;
+
+    case 2:
+        Enemy_healing(k);
+        break;
+
+    case 3:
+        Enemy_item(k);
+        break;
+    }
+}
+
